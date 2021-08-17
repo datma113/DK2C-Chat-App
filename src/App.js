19f0e23e52ebd58781473e173923e27cae9f4d3f
datma <datma113112111@gmail.com>
2021-08-17 10:20:07 +0700

    .

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './css/styles.css'
 
 function App() {
 
- 
+
 
   return (
     <div className="App">
         <div className="card bg">
         <div className="card__header">header</div>
         </div>
-        <i class="fas fa-angle-double-down"></i>
+        <i className="fas fa-angle-double-down"></i>
     </div>
   );
 }

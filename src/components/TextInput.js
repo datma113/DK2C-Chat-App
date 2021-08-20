a78a63bf134d9e6ead2dcca4dea0cf6bbdd506dc
datma <datma113112111@gmail.com>
2021-08-20 11:49:30 +0700

    change register code

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,31 +1,40 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
-const TextInput = ({ id, label, type, checkRegex, regexPattern, functionToDispatch, keyStoreToReducer }) => {
-    const dispatch = useDispatch()
+const TextInput = ({
+    id,
+    label,
+    type,
+    checkRegex,
+    regexPattern,
+    functionToDispatch,
+    keyStoreToReducer,
+}) => {
+    const dispatch = useDispatch();
     const [messageOfRegex, setmessageOfRegex] = useState("");
 
     const isShowMessageOfRegex = (message) => {
         return message.length === 0 ? "d-none" : "";
     };
 
-    /** 
+    /**
      * This function can catch an undefined function pass as props
      */
     const isValidationOfCheckRegexProp = (valueOfInput) => {
-        const IS_VALID = typeof checkRegex(valueOfInput, regexPattern)
-        return IS_VALID === "string" ? true : false
-    }
+        const IS_VALID = typeof checkRegex(valueOfInput, regexPattern);
+        return IS_VALID === "string" ? true : false;
+    };
 
     const changeStateWhenTriggered = (valueOfInput) => {
-        if(isValidationOfCheckRegexProp(valueOfInput))
+        if (isValidationOfCheckRegexProp(valueOfInput))
             setmessageOfRegex(checkRegex(valueOfInput, regexPattern));
-        else setmessageOfRegex("")
-    }
+        else setmessageOfRegex("");
+    };
 
     const dispatchUserLoginToStore = (valueOfInput) => {
-        dispatch(functionToDispatch(keyStoreToReducer, valueOfInput))
-    }
+        if (functionToDispatch !== undefined)
+            dispatch(functionToDispatch(keyStoreToReducer, valueOfInput));
+    };
 
     return (
         <div className="input-container mb-5">
@@ -36,14 +45,18 @@ const TextInput = ({ id, label, type, checkRegex, regexPattern, functionToDispat
                 id={id}
                 autoComplete="off"
                 onChange={(e) => {
-                    changeStateWhenTriggered(e.target.value)
-                    dispatchUserLoginToStore(e.target.value)
+                    changeStateWhenTriggered(e.target.value);
+                    dispatchUserLoginToStore(e.target.value);
                 }}
             />
             <label htmlFor={id} className="input-container__label">
                 {label}
             </label>
-            <p className={`text-danger text-message-danger ${isShowMessageOfRegex(messageOfRegex)}`}>
+            <p
+                className={`text-danger text-message-danger ${isShowMessageOfRegex(
+                    messageOfRegex
+                )}`}
+            >
                 {" "}
                 {messageOfRegex}{" "}
             </p>
--- src/pages/welcome/Register.js
+++ src/pages/welcome/Register.js
@@ -1,59 +1,60 @@
-import React from 'react'
+import React from "react";
 import logo from "../../assets/image/LOGO.png";
-import { Form, Formik, useFormik } from 'formik';
-import * as Yup from 'yup';
-import FormikControl from '../../components/customForm/FormikControl'
+import MyCustomButton from "../../components/MyCustomButton";
+import TextInput from "../../components/TextInput";
+import regexInputModule from "../../module/regexInputModule";
+
 const Register = () => {
 
-  return (
-    <Formik initialValues={{ phoneNumber: '' , fullName: '', email:''}}
-      validationSchema={Yup.object({
-        phoneNumber: Yup.string()
-          .matches(/^[0-9]*$/, "Số điện thoại phải là số!")
-          .max(10, 'Số điện thoại quá dài')
-          .min(10, 'Số điện thoại quá ngắn')
-          .required('Số điện thoại là bắt buộc')
-        ,
-          fullName: Yup.string()
-          .max(20, 'Tên tối đa 20 chữ cái')
-          .required('Tên là bắt buộc!'),
-        email: Yup.string().email('Địa chỉ email không hợp lệ').required('Email là bắt buộc!'),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        alert(JSON.stringify(values, null, 2));
-        // setSubmitting(false);
-      }}>
-      <Form >
-        <div className="d-flex justify-content-center mt-5">
-          <div className="col-lg-4 d-flex flex-column align-items-center justify-content-center welcome-container">
-            <img src={logo} alt="" className="welcome-container__logo " />
-            <p className="text-title mt-3">Đăng ký</p>
-            <div className="mt-3 col-6 ">
-              <FormikControl
-                control="input"
-                label="Số điện thoại:"
-                name="phoneNumber"
-                type="string"
-              />
-              <FormikControl
-                control="input"
-                label="Họ và tên:"
-                name="fullName"
-                type="string"
-              />
-              <FormikControl
-                control="input"
-                label="email:"
-                name="email"
-                type="string"
-              />
-            </div>
-            <button className="btn btn-secondary btn-welcome" type="submit">Đăng ký</button>
-          </div>
-        </div>
-      </Form>
-    </Formik>
-  )
-}
+  const REGISTER_FIELDS = [
+    { label: "Họ tên", type: "text", regexPattern: /[\D]/g, keyStoreToReducer: "fullName" },
+    { label: "Email", type: "text", regexPattern: /[\D]/g, keyStoreToReducer: "email" },
 
-export default Register
+    { label: "Số điện thoại", type: "text", regexPattern: /[\D]/g, keyStoreToReducer: "phone" },
+    {
+        label: "Mật khẩu",
+        type: "password",
+        regexPattern: /[\w]/g,
+        keyStoreToReducer: "password",
+    },
+];
+
+const registerMap = REGISTER_FIELDS.map((field, index) => {
+    let checkRegex = function () {};
+    switch (index) {
+        case 0:
+            checkRegex = regexInputModule.checkRegexOfUserLoginPhone;
+            break;
+        case 1:
+            checkRegex = regexInputModule.checkRegexOfUserPassword;
+            break;
+        default:
+            break;
+    }
+    return (
+        <TextInput
+            key={index}
+            id={index}
+            label={field.label}
+            type={field.type}
+            checkRegex={checkRegex}
+            regexPattern={field.regexPattern}
+            
+      
+        />
+    );
+});
+
+    return (
+        <div className="d-flex justify-content-center mt-5">
+            <div className="col-lg-4 d-flex flex-column align-items-center justify-content-center welcome-container">
+                <img src={logo} alt="" className="welcome-container__logo " />
+                <p className="text-title mt-3">Đăng ký</p>
+                <div className="mt-3 col-6 ">{registerMap}</div>       
+                <MyCustomButton label="đăng Ký" typeButton="secondary" />
+            </div>
+        </div>
+    );
+};
+
+export default Register;

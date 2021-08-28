c36b9c2ba494a128a371ae6e1033c60f3adab8c3
datma <datma113112111@gmail.com>
2021-08-28 21:49:25 +0700

    test feature get inbox

--- src/pages/homePage/InboxList.js
+++ src/pages/homePage/InboxList.js
@@ -1,9 +1,18 @@
+import axios from 'axios'
 import React from 'react'
 
 const InboxList = () => {
+
+     const getInbox = () => {
+          axios.get('http://localhost:8080/api/inboxs')
+          .then((resp) => {
+               console.log(resp.data)
+          })
+     }
+
      return (
           <div>
-               api/inboxs
+               <div className="btn btn-dark btn-welcome" onClick={getInbox}>get inbox</div>
           </div>
      )
 }

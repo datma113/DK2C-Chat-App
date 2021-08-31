736cea4ef58f7adb51717d0d456e9520bab2b9ee
datma <datma113112111@gmail.com>
2021-08-31 21:57:53 +0700

    split boxchat into  component

---
+++ src/components/MessageChat.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const MessageChat = ({ boxChat }) => {
+    const authentication = useSelector((state) => state.authentication);
+
+    const boxChatMap = boxChat.map((message, index) => {
+        const SENDER_ID = message.sender.id;
+        const MY_ID = authentication.user.id;
+        const IS_SELF_SIDE = SENDER_ID === MY_ID ? true : false;
+        const getSelfSideClass = () => {
+            return IS_SELF_SIDE ? "" : "single-chat-box--other";
+        };
+
+        return (
+            <div key={index} className={`single-chat-box mb-1 ${getSelfSideClass()}`}>
+                <div>
+                    <img
+                        className="single-chat-box__img m-3"
+                        src={message.sender.imageUrl}
+                        alt=""
+                    />
+                </div>
+                <div className="single-chat-box__message mt-3"> {message.content} </div>
+            </div>
+        );
+    });
+    return <div>{boxChatMap}</div>;
+};
+
+export default MessageChat;
--- src/pages/homePage/BoxChat.js
+++ src/pages/homePage/BoxChat.js
@@ -2,38 +2,17 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AlwaysScrollToBottom from "../../components/AlwaysScrollToBottom";
+import MessageChat from "../../components/MessageChat";
 import { getMessageInBoxChat } from "../../redux/action/actHome";
 
 const BoxChat = () => {
     const dispatch = useDispatch();
     const boxChat = useSelector((state) => state.boxChat);
     const currentIdBoxChat = useSelector((state) => state.currentIdBoxChat);
-    const authentication = useSelector((state) => state.authentication);
     const [loadingOlderMessage, setloadingOlderMessage] = useState(0);
     const [isInitialize, setisInitialize] = useState(true);
     const [lenthOfTheFirstLoadingMessage, setlenthOfTheFirstLoadingMessage] = useState(0);
-
-    const boxChatMap = boxChat.map((message, index) => {
-        const SENDER_ID = message.sender.id;
-        const MY_ID = authentication.user.id;
-        const IS_SELF_SIDE = SENDER_ID === MY_ID ? true : false;
-
-        const getSelfSideClass = () => {
-            return IS_SELF_SIDE ? "" : "single-chat-box--other";
-        };
-
-        return (
-            <div key={index} className={`single-chat-box mb-1 ${getSelfSideClass()}`}>
-                {" "}
-                <img
-                    className="single-chat-box__img m-3"
-                    src={message.sender.imageUrl}
-                    alt=""
-                />{" "}
-                <div className="single-chat-box__message mt-3"> {message.content} </div>
-            </div>
-        );
-    });
+   
 
     useEffect(() => {
         dispatch(getMessageInBoxChat(currentIdBoxChat, 0));
@@ -59,7 +38,7 @@ const BoxChat = () => {
 
     return (
         <div className="single-chat-box-container" onScroll={(e) => loadOlderMessageInBoxChat(e)}>
-            {boxChatMap}
+            <MessageChat boxChat={boxChat} />
             {isInitialize && <AlwaysScrollToBottom />}
         </div>
     );

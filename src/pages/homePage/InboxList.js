1ec646fd8a9f79f749c3898b3dc9f037485fd2fc
LuuTuanKha <74186947+LuuTuanKha@users.noreply.github.com>
2021-10-02 21:04:32 +0700

    addfriend fix

--- src/pages/homePage/InboxList.js
+++ src/pages/homePage/InboxList.js
@@ -34,7 +34,7 @@ const InboxList = () => {
         const TYPE_ROOM_GROUP = "GROUP";
         const IS_ACTIVE = currentInboxId === inbox.id;
         const CURRENT_ROOM_ID = inbox.room.id;
-       
+        
         const MY_ID = authentication.user.id;
         const SENDER_ID = inbox.lastMessage.sender.id;
         const IS_MYSELF = MY_ID === SENDER_ID ? true : false;

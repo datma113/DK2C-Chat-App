08841079f0ffd59b3d6f21957487f912d2e9bc6d
LuuTuanKha <74186947+LuuTuanKha@users.noreply.github.com>
2021-10-14 20:32:18 +0700

    Update ProfileDetail.js

--- src/pages/profile/ProfileDetail.js
+++ src/pages/profile/ProfileDetail.js
@@ -7,12 +7,12 @@ const ProfileDetail = (props) => {
         <div className="container row ">
             <div className="profile__detail col-12">
                 <h5>Số điện thoại đăng ký:</h5>
-                <input value={user.phoneNumber} disabled />
+                <input value={user.phoneNumber || 'đang tải'} disabled />
                 <br />
                 <h5>Email:</h5>
-                <input value={user.email} disabled />
+                <input value={user.email || 'đang tải'} disabled />
                 <h5>Số điện thoại đăng ký:</h5>
-                <input value={user.phoneNumber} disabled />
+                <input value={user.phoneNumber || 'đang tải'} disabled />
             </div>
             
 

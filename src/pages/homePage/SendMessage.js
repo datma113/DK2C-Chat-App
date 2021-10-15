4b6aff131afa1dbc99e45726db0422082464e55d
datma <datma113112111@gmail.com>
2021-10-15 14:51:28 +0700

    add emoji in chat

--- src/pages/homePage/SendMessage.js
+++ src/pages/homePage/SendMessage.js
@@ -3,10 +3,18 @@ import { useDispatch } from "react-redux";
 import socketModule from "../../module/socketModule";
 import { SCROLL_BOTTOM_WHEN_SEND_MESSAGE } from "../../redux/constants/constants";
 import SendImage from "./SendImage";
+import Picker, { SKIN_TONE_MEDIUM_DARK } from "emoji-picker-react";
 
 const SendMessage = ({ roomId }) => {
     const [messageToSend, setmessageToSend] = useState("");
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+    const [isShowEmojiExpress, setisShowEmojiExpress] = useState(false);
     const dispatch = useDispatch();
+
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+        setmessageToSend(messageToSend + " " + emojiObject.emoji);
+    };
     const sendMessageToFriend = () => {
         if (messageToSend.length) {
             socketModule.sendMessageToOneFriend(roomId, messageToSend, "TEXT");
@@ -24,13 +32,19 @@ const SendMessage = ({ roomId }) => {
             e.preventDefault();
             socketModule.sendMessageToOneFriend(roomId, messageToSend, "TEXT");
             setmessageToSend("");
+            setisShowEmojiExpress(false);
             dispatch({
                 type: SCROLL_BOTTOM_WHEN_SEND_MESSAGE,
                 status: true,
             });
         }
+        
+        if (e.key === "Escape") {
+            setisShowEmojiExpress(false);
+        }
     };
 
+    const toggleEmojiExpressShowed = (currentStatus) => setisShowEmojiExpress(!currentStatus);
     return (
         <div className="send-message-container">
             <textarea
@@ -41,8 +55,26 @@ const SendMessage = ({ roomId }) => {
                 onKeyDown={(e) => handleEnterTextarea(e)}
             />
             <div className="center send-message-container__icon-container">
-                <SendImage roomId={roomId}/>
-                <i className="fab fa-telegram-plane" onClick={() => sendMessageToFriend()}></i>
+                <SendImage roomId={roomId} />
+                <i
+                    className="far fa-grin-tongue-squint text-warning"
+                    onClick={() => toggleEmojiExpressShowed(isShowEmojiExpress)}
+                ></i>
+                <div
+                    className={`send-message-container__icon-container__emoji-express ${
+                        isShowEmojiExpress ? "" : "d-none"
+                    } `}
+                >
+                    <Picker
+                        onEmojiClick={onEmojiClick}
+                        skinTone={SKIN_TONE_MEDIUM_DARK}
+                        disableSearchBar={true}
+                    />
+                </div>
+                <i
+                    className="fas fa-angle-double-right text-primary"
+                    onClick={() => sendMessageToFriend()}
+                ></i>
             </div>
         </div>
     );

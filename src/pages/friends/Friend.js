552195302d4f741781e77bd09d4b8e2078cd1c2d
datma <datma113112111@gmail.com>
2021-10-26 10:19:59 +0700

    click to show friend options

--- src/pages/friends/Friend.js
+++ src/pages/friends/Friend.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Friend = ({ friend, functionWhenClick, gotoChat }) => {
+const Friend = ({ friend, functionWhenClick, gotoChat, clickToOption, isShowFriendsOption }) => {
     return (
         <div className="friend row p-3 center" onClick={functionWhenClick}>
             <div className="col-3 center">
@@ -14,20 +14,20 @@ const Friend = ({ friend, functionWhenClick, gotoChat }) => {
             </div>
 
             <div
-                className="col-1 option-friend-inbox"
-                onClick={(e) => {
-                    e.stopPropagation();
-                    console.log(e.target, this);
-                }}
+                className="col-1 option-friend-inbox "
+                id={`fo${friend.friend.id}`}
+                onClick={clickToOption}
             >
-                <div className="center flex-column option-friend-inbox__options">
+                <div
+                    className={`center flex-column option-friend-inbox__options ${isShowFriendsOption}`}
+                >
                     <div
-                        className="option-friend-inbox__options__element text-small"
+                        className={`option-friend-inbox__options__element  text-small`}
                         onClick={gotoChat}
                     >
                         Nhắn tin
                     </div>
-                    <div className="option-friend-inbox__options__element text-small">
+                    <div className={`option-friend-inbox__options__element text-small`}>
                         xem thông tin
                     </div>
                 </div>
--- src/pages/friends/ListFriends.js
+++ src/pages/friends/ListFriends.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { getInboxByFriendId } from "../../redux/action/actFriends";
@@ -6,19 +6,41 @@ import Friend from "./Friend";
 
 const ListFriends = () => {
     const friendsList = useSelector((state) => state.friendsList);
+    const [indexOfFriendsOption, setindexOfFriendsOption] = useState(-1);
     const dispatch = useDispatch();
 
     const gotoChatboxByFriendId = (friendId) => {
         dispatch(getInboxByFriendId(friendId));
     };
 
+    const handleClickToOption = (e, index) => {
+        e.stopPropagation();
+
+        setindexOfFriendsOption(index);
+    };
+
+    const isShowfriendsOption = (index) => {
+        return index === indexOfFriendsOption ? "option-friend-inbox__options--show" : "";
+    };
+
+    window.onclick = () => {
+        setindexOfFriendsOption(-1);
+    };
+
     const listFriendMap = friendsList.map((friend, index) => {
+        const ID = friend.friend.id;
         return (
             <Friend
                 key={index}
                 friend={friend}
-                functionWhenClick={() => gotoChatboxByFriendId(friend.friend.id)}
-                gotoChat={() => gotoChatboxByFriendId(friend.friend.id)}
+                functionWhenClick={() => gotoChatboxByFriendId(ID)}
+                gotoChat={() => {
+                    dispatch(getInboxByFriendId(ID)).then(() => setindexOfFriendsOption(-1));
+                }}
+                clickToOption={(e) => {
+                    handleClickToOption(e, index);
+                }}
+                isShowFriendsOption={isShowfriendsOption(index)}
             />
         );
     });
